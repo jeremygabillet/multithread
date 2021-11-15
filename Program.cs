201c76ce511680eb802b526b8db3f97f423d712e
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-15 10:14:27 +0100

    Finishing quest

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace multithread
 {
@@ -6,7 +8,29 @@ namespace multithread
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.Run();
+        }
+        private void Run()
+        {
+            ThreadStart threadStart = new ThreadStart(OnThreadStart);
+            List<Thread> threads = new List<Thread> 
+            {
+                new Thread(threadStart),
+                new Thread(threadStart)
+            };
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+        }
+        private void OnThreadStart()
+        {
+            Random random = new Random();
+            int randSleep = random.Next(1000, 5000);
+            Thread.Sleep(randSleep);
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            System.Console.WriteLine($" Thread n° {threadId} finished in {randSleep} milliseconds");
         }
     }
 }
